81634ece404f51fe6acc815ccd123b0279001ad7
Peter Hoffmann <Hoffmann.P@gmx.net>
2012-04-03 20:52:30 +0200

    mysqlTunnel now supports killing mysqld (and other programms you need to be root to stop)

--- mysqlTunnel.sh
+++ mysqlTunnel.sh
@@ -5,9 +5,17 @@ Port=3306
 # server.csv is a file containing in the first column your servers name (for your eyes) and the servers address (for your computer)
 SF="bin/server.csv";
 
+TEXT_KillMysqlf="Eine andere Instanz dieses Programms läuft bereits. Soll diese beendet werden? (ansonsten wird diese Aktion abgebrochen)";
+TEXT_Rootspace=" welches nur durch mit Rootrechten beendet werden kann";
+TEXT_Userspace=" [%i]";
+TEXT_KillElseone="Der Port %i ist bereits durch anderes Programm belegt %s. Soll dieses Programm beendet werden? (ansonsten wird diese Aktion abgebrochen)";
+TEXT_KillFail="Das Programm %i konnte nicht beendet werden. Die Aktion wird abgebrochen!";
+TEXT_Server="Server";
+TEXT_ChooseServer="$TEXT_Server wählen";
+
 running=$(ps auxf | grep "$Port:localhost:$Port" | grep -v grep | tr -s '[:blank:]' '\t' | cut -f 2)
 if [[ "$running" ]]; then
-	zenity --question --text="Eine andere Instanz dieses Programms läuft bereits. Soll diese abgrebochen werden? (ansonsten wird diese Aktion abgebrochen)";
+	zenity --question --text="$TEXT_KillMysqlf";
 	if [[ $? == 0 ]]; then
 		kill $running;
 	else
@@ -20,27 +28,43 @@ active=$(netstat -tan 2>/dev/null | grep ":$Port" | wc -l);
 if [[ $active > 0 ]]; then
 	PID="$(netstat -ptan 2>/dev/null | grep -m 1 ":$Port" | tr -s '[:blank:]*' '\t' | cut -f 7)";
 	if [[ "$PID" == "-" ]]; then
-		zenity --error --text="Der Port $Port ist bereits durch anderes Programm belegt welches nicht beendet werden kann. Die Aktion wird abgebrochen!";
-		echo "port is busy (not killable by user)" >&2;
-		exit 1;
-	fi;
-	
-	zenity --question --text="Der Port $Port ist bereits durch anderes Programm belegt [$PID]. Soll dieses Programm beendet werden? (ansonsten wird die Aktion abgebrochen)";
-	if [[ $? == 1 ]]; then
-		echo "port is busy (don't want to kill)" >&2;
-		exit 2;
-	fi;
-
-	kill $(echo $PID | cut -d '/' -f 1);
-	active=$(netstat -tan 2>/dev/null | grep ":$Port" | wc -l);
-	if [[ $active > 0 ]]; then
-		zenitry --error --text="Das Programm $PID konnte nicht beendet werden. Die Aktion wird abgebrochen!";
-		echo "port is busy (could not kill)" >&2;
-		exit 3;
+		zenity --question --text="$(printf "$TEXT_KillElseone" $Port "$TEXT_Rootspace")";
+        if [[ $? == 0 ]]; then
+			function killRootProgramm() {
+				PID="$(netstat -ptan 2>/dev/null | grep -m 1 ":$Port" | tr -s '[:blank:]*' '\t' | cut -f 7)";
+				kill $(echo $PID | cut -d '/' -f 1);
+				active=$(netstat -tan 2>/dev/null | grep ":$Port" | wc -l);
+				if [[ $active > 0 ]]; then
+					zenitry --error --text="$(printf "$TEXT_KillFail" $PID)";
+					echo "port is blocked - can't unblock" >&2;
+					exit 3;
+				fi;
+			}
+			gksu killRootProgramm;
+			echo "port successfully unblocked";
+		else
+			echo "port is blocked - need to be root to unblock" >&2;
+			exit 1;
+		fi;
+	else
+		zenity --question --text="$(printf "$TEXT_KillElseone" $Port "$(printf $TEXT_Userspace $PID)")";
+		if [[ $? == 0 ]]; then
+			kill $(echo $PID | cut -d '/' -f 1);
+			active=$(netstat -tan 2>/dev/null | grep ":$Port" | wc -l);
+			if [[ $active > 0 ]]; then
+				zenitry --error --text="$(printf $TEXT_KillFail $PID)";
+				echo "port is blocked - can't unblock" >&2;
+				exit 3;
+			fi;
+			echo "port successfully unblocked";
+		else
+			echo "port is blocked - don't want to unblock" >&2;
+			exit 2;
+		fi;
 	fi;
 fi;
 
-Server=$(zenity --list --text "Server wählen" --radiolist --column="✓" --column="Server" $(for S in $(cut -d, -f1 "$SF"); do echo "0 $S "; done));
+Server=$(zenity --list --text "$TEXT_ChooseServer" --radiolist --column="✓" --column="$TEXT_Server" $(for S in $(cut -d, -f1 "$SF"); do echo "0 $S "; done));
 
 if [[ "$Server" == "" ]]; then
 	echo "No server selected" >&2;
@@ -48,6 +72,6 @@ if [[ "$Server" == "" ]]; then
 fi;
 
 host=$(grep "^$Server," "$SF" | cut -d, -f2)
- 
-echo "ssh -c blowfish -NL $Port:localhost:$Port $host &"
+
+echo "ssh -c blowfish -NL $Port:localhost:$Port $host &";
 ssh -c blowfish -NL $Port:localhost:$Port $host
