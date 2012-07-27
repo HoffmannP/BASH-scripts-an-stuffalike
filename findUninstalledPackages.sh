e3cd5b13c646750c44c720a8f524e9cc0e07392d
Peter Hoffmann <Hoffmann.P@gmx.net>
2012-07-27 12:53:53 +0200

    Jetzt?

--- findUninstalledPackages.sh
+++ findUninstalledPackages.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-dpkg -l | grep ^ii | tr -s ' ' "\t" | cut -f2 | tee packageList.txt
+dpkg -l | grep ^ii | tr -s ' ' "\t" | cut -f2 | tee allPackages.txt
 
+rm dependencyList.txt
 touch dependencyList.txt
-for package in $(cat packageList.txt); do
+for package in $(cat allPackages.txt); do
 	apt-cache -i depends $package | sed -n '/^ .Hängt[^:]*: [^<]/{s/^ .Hängt[^:]*: //;p}' | tee -a dependencyList.txt
 done;
 sort -u dependencyList.txt | tee dependencyList.tmp.txt
 mv dependencyList.tmp.txt dependencyList.txt
 
-touch installedPackages.txt
-for package in $(cat packageList.txt); do
+rm directPackages.txt
+touch directPackages.txt
+for package in $(cat allPackages.txt); do
 	if [[ $(grep -c $package dependencyList.txt) -eq 0 ]]; then
-		echo $package  | tee -a installedPackages.txt;
+		echo $package  | tee -a directPackages.txt;
 	fi;
 done;
