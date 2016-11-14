4aef7268a2d11d6b04eb5634efdff66443e50b30
Berengar W. Lehr <Hoffmann.P@gmx.net>
2016-11-14 22:02:41 +0100

    Gelintet

--- wallpaper.py
+++ wallpaper.py
@@ -3,37 +3,61 @@
 
 """ 1680x1050 "spannen" vs 3360x1050 "einfach"  """
 
+from subprocess import call
+from random import randint as rnd
 from bs4 import BeautifulSoup as bs4
 from requests import get, Session
-from random import randint as rnd
-from subprocess import call
 
 
-wallpaperPrefix = '/home/ber/.wallpaper'
-sfx = '.jpg'
-baseUrl = 'http://wallpaperswide.com'
-resolution = '1680x1050'
-desktop = 'cinnamon'
-callSetting = {
-    'mate': lambda fn: call(['/usr/bin/gsettings', 'set', 'org.mate.background', 'picture-filename', fn]),
-    'cinnamon': lambda fn: call(['/usr/bin/gsettings', 'set', 'org.cinnamon.desktop.background', 'picture-uri', 'file://' + fn])
+WALLPAPER_PREFIX = '/home/ber/.wallpaper'
+SFX = '.jpg'
+BASE_URL = 'http://wallpaperswide.com'
+RESOLUTION = '1680x1050'
+DESKTOP = 'cinnamon'
+CALL_SETTING = {
+    'mate': lambda fn: call([
+        '/usr/bin/gsettings',
+        'set',
+        'org.mate.background',
+        'picture-filename',
+        fn]),
+    'cinnamon': lambda fn: call([
+        '/usr/bin/gsettings',
+        'set',
+        'org.cinnamon.desktop.background',
+        'picture-uri',
+        'file://' + fn])
 }
+IGNORED_CATEGORIES = [
+    'Aero',
+    'Army',
+    'Funny',
+    'Movies',
+    'Models',
+    'Girls',
+    'Cute',
+    'Motors',
+    'Celebrities',
+    'Games',
+    'Sports']
 
-
-def maxPage():
-    request = get(baseUrl + '/' + resolution + '-wallpapers-r.html')
+def max_page():
+    """ number of current pages """
+    request = get(BASE_URL + '/' + RESOLUTION + '-wallpapers-r.html')
     page = bs4(request.text, "lxml")
     return int(page.select('div.pagination a')[5].text)
 
-
-def imageSelect():
-    url = baseUrl + '/' + resolution + '-wallpapers-r/page/' + str(rnd(1, maxPage()))
+def image_select():
+    """ randomly return a image url """
+    url = BASE_URL + '/' + RESOLUTION + '-wallpapers-r/page/' + str(rnd(1, max_page()))
     page = bs4(get(url).text, "lxml")
     return page.select('li.wall div.thumb a')[2*rnd(0, 9)]['href']
 
 
-def getReferer(url, referer):
-    useragent = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.14) Gecko/20080418 Ubuntu/7.10 (gutsy) Firefox/2.0.0.14'
+def get_referer(url, referer):
+    """ creates the referer object """
+    useragent = 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.14)'+\
+    ' Gecko/20080418 Ubuntu/7.10 (gutsy) Firefox/2.0.0.14'
     session = Session()
     session.headers.update({
         'referer': referer,
@@ -42,26 +66,28 @@ def getReferer(url, referer):
     return session.get(url)
 
 
-def categorieIgnore(url, ignoreCategories):
+def categorie_ignore(url, ignore_categories):
+    """ ignores defined categories of wallappers  """
     page = bs4(get(url).text, "lxml")
     categories = page.select('#content ul')[1].select('a')
     for categorie in categories:
-        if categorie.get_text().split('/')[0] in ignoreCategories:
+        if categorie.get_text().split('/')[0] in ignore_categories:
             return True
     return False
 
 
-def setWallpaper():
+def set_wallpaper():
+    """main function to randomly set the wallpaper without ignored categories"""
     while True:
-        page = imageSelect()
-        urlPage = baseUrl + page
-        if not categorieIgnore(urlPage, ['Models', 'Girls', 'Cute', 'Motors', 'Celebrities', 'Games']):
+        page = image_select()
+        url_page = BASE_URL + page
+        if not categorie_ignore(url_page, IGNORED_CATEGORIES):
             break
-    urlImage = baseUrl + '/download' + page[:-len('s.html')] + '-' + resolution + sfx
-    fileName = wallpaperPrefix + str(rnd(0, 1E5)) + sfx
-    with open(fileName, 'w') as f:
-         f.write(getReferer(urlImage, urlPage).content)
-    callSetting[desktop](fileName)
+    url_image = BASE_URL + '/download' + page[:-len('s.html')] + '-' + RESOLUTION + SFX
+    file_name = WALLPAPER_PREFIX + str(rnd(0, 1E5)) + SFX
+    with open(file_name, 'w') as fi:
+        fi.write(get_referer(url_image, url_page).content)
+    CALL_SETTING[DESKTOP](file_name)
 
 if __name__ == '__main__':
-    setWallpaper()
+    set_wallpaper()
