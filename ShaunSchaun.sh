4be3ad40dc6d3e1a5449e2721f8f42e0afafe5f0
Berengar W. Lehr <Hoffmann.P@gmx.net>
2018-09-24 20:27:58 +0200

    Noch besseres Shaun Script

--- ShaunSchaun.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd "/home/ber/Videos/Kinder/Shaun The Sheep"
-/usr/bin/vlc Season\ 0[345]/*
---
+++ ShaunShaun.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/bin/vlc --random /home/ber/Videos/Kinder/Shaun\ The\ Sheep/Season\ 0[345]/*
