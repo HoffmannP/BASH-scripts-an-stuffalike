b997b46585a41246ea1a87e47c0bc2e6e5eb4ce1
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2021-12-10 19:12:10 +0100

    Extrahiert jetzt auch den richtigen Dateinamen

--- extractAttachment.sh
+++ extractAttachment.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# TODO: Unicode-Handling von Mailbody und Dateinamen
+
 mail="$1"
 
-boundary=$(rg -i ^Content-Type: "$mail" | head -1 | cut -d= -f2 | tr -d '\r')
+boundary=$(rg -i ^Content-Type: "$mail" | head -1 | cut -d= -f2 | tr -d '"\r')
 files=$(csplit --prefix=.attachments "$mail" "/^--$boundary/" '{*}' | wc -l)
 if [[ $files -lt 2 ]]
 then
@@ -11,7 +13,7 @@ then
 fi
 
 bodyfile="${mail:0:-3}txt"
-boundary=$(rg -i ^Content-Type: .attachments01 | head -1 | cut -d= -f2 | tr -d '\r')
+boundary=$(rg -i ^Content-Type: .attachments01 | head -1 | cut -d= -f2 | tr -d '"\r')
 if [[ -n "$boundary" ]]
 then
     text=$(csplit --prefix=.mailtext .attachments01 "/^--$boundary/" '{*}' | wc -l)
@@ -24,14 +26,20 @@ else
 fi
 echo "$bodyfile"
 
-for ((file=2; file<$[files-1]; file++)) {
+for ((file=2; file<$files; file++)) {
     attachment=$(printf ".attachments%02d" $file)
-    filename=$(rg -i '^Content-Disposition:' $attachment | cut -d= -f2 | tr -d '"\r' | tr -d "'")
-    # cat .attachments02 | awk '/Content-Disposition/{ line=$0; while (substr($0, length($0) - 1, 1) == ";") { getline; line=line$0 }; print line }'
+    filename=$(cat $attachment |\
+        awk '{ if ($0 ~ /^Content\-Disposition:/) { while (substr($0, length($0) - 1, 1) == ";") { printf $0 |"tr -d \r"; getline } printf $0 | "tr -d \r" } }' |\
+        awk 'BEGIN { FS="=" }{ print $NF }' |\
+        tr -d '"' | tr -d "'")
+    if [[ -z "$filename" ]]
+    then
+        continue
+    fi
     filename=$(basename "$filename") # only for security
     cat $attachment |\
         awk 'BEGIN{ head=1 }{ if (head == 0) { if (length($0) < 2) { head=1 } else { print $0 } } else { if (length($0) < 2) { head=0 } } }' |\
         sed 's/\r$//' | base64 -d > "$filename"
     echo "$filename"
 }
-# rm .attachments*
+rm .attachments*
