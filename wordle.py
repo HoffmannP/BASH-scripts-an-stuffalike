0185e369b779384d54045f3eec90017be6e3150d
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2022-01-24 11:35:54 +0100

    Wordle

--- wordle.py
+++ wordle.py
@@ -1,14 +1,16 @@
 #/usr/bin/env python3
 
-fixed = '_R'
+import words
+
+fixed = 'S__ER'
 
 found = [
-    ['c', 0 ]
-    # [ Letter, Places … ]
+    # ['e', 1 ],
+    # ['r', 2 ]
 ]
 
 remaining = ''
-excluded = ''
+excluded = 'otin'
 
 
 # -----
@@ -19,7 +21,6 @@ def iter(outer=['']):
 
 CHARACTERS = 5
 ALLLETTERS = set('abcdefghijklmnopqrstuvwxyz')
-MUST_CONTAIN = set('aeiou')
 
 fixed += '_____'
 
@@ -30,17 +31,20 @@ else:
 letters = baseset - {e.lower() for e in excluded}
 iterator = iter()
 letter = [None] * CHARACTERS
+must_contain = { f[0] for f in found }
 
 for position in range(CHARACTERS):
     if fixed[position].lower() in ALLLETTERS:
         possible = {fixed[position].lower()}
     else:
         possible = letters - {f[0].lower() for f in found if position in f[1:]}
-    print(position, possible)
+    print(position, ''.join(possible))
     iterator = iter(iterator(possible))
 
 for word in iterator():
-    if MUST_CONTAIN & set(word) == set():
-        continue
-
-    print(word, end='\t')
+    for char in must_contain:
+        if char not in word:
+            break
+    else:
+        if word in words.de:
+            print(word, end='\t')
