80ac1f0553a25c1fe60401278d1e6e7307772365
Peter Hoffmann <Hoffmann.P@gmx.net>
2024-03-26 00:20:33 +0100

    Jetzt mit Dict

--- eztv.api.py
+++ eztv.api.py
@@ -1,10 +1,12 @@
 #!env python
 
 import subprocess
-import requests
-import simple_term_menu
 import json
 import os.path
+import sys
+
+import requests
+import simple_term_menu
 
 LIMIT=100
 BEST_SIZE=1.7*2**10
@@ -32,12 +34,15 @@ def till_field(stream, **kwargs):
                 return
         yield torrent
 
-def filter_minimum(stream, **kwargs):
+def filter_field(stream, **kwargs):
     for torrent in stream:
+        do_yield = True
         for key, value in kwargs.items():
             if torrent[key] < value:
-                continue
-        yield torrent
+                do_yield = False
+                break
+        if do_yield:
+            yield torrent
 
 def group_by_se(stream):
     torrents = {}
@@ -59,10 +64,9 @@ def best_size(torrents):
 
 def episode_list(imdb_id, season, episode=0):
     return { se: best_size(ts) for se, ts in sorted(group_by_se(
-        filter_minimum(
-            till_field(
-                load_show(imdb_id),
-                se=MAX_EP_PER_SE * season + episode),
+        filter_field(
+            load_show(imdb_id),
+            se=MAX_EP_PER_SE * season + episode,
             seeds=1,
             size=300)
         ).items()) }
@@ -88,29 +92,82 @@ def nice_se(se):
     se, ep = se // MAX_EP_PER_SE, se % MAX_EP_PER_SE
     return f'{se}x{ep}'
 
-def main():
+def load_list():
+    showlist_path = f'{os.path.dirname(__file__)}/{SHOWLIST_FILE}'
+    keys = [ 'name', 'imdb_id', 'season', 'episode' ]
+    with open(showlist_path, 'r', encoding='utf8') as file:
+        return [ { keys[i]: col for i, col in enumerate(show) } for show in json.load(file) ]
+
+def load_dict():
     showlist_path = f'{os.path.dirname(__file__)}/{SHOWLIST_FILE}'
     with open(showlist_path, 'r', encoding='utf8') as file:
-        showlist = json.load(file)
-    show_selection = simple_term_menu.TerminalMenu([name for name, _, _, _ in showlist])
-    show_index = show_selection.show()
-    show = showlist[show_index]
-    print(show[0])
-    episodes = episode_list(*show[1:])
+        return json.load(file)
+
+def load():
+    return load_dict()
+
+def select_show(showlist, select=None):
+    show_names = [show['name'] for show in showlist]
+    if select in show_names:
+        show_index = show_names.index(select)
+    else:
+        show_selection = simple_term_menu.TerminalMenu([show['name'] for show in showlist])
+        show_index = show_selection.show()
+    if show_index is None:
+        return None, None
+    return show_index, showlist[show_index]
+
+def select_episode(show):
+    episodes = episode_list(show['imdb_id'], show['season'], show['episode'])
     episode_keys = list(episodes.keys())
     episode_selection = simple_term_menu.TerminalMenu([nice_se(i) for i in episode_keys])
     episode_index = episode_selection.show()
+    if episode_index is None:
+        return None, None, None
     episode_key = episode_keys[episode_index]
-    print(nice_se(episode_key))
-    if episode_index > 0:
-        episode_prev = list(episodes.keys())[episode_index - 1]
-        showlist[show_index][2] = episode_prev // MAX_EP_PER_SE
-        showlist[show_index][3] = episode_prev % MAX_EP_PER_SE
-        with open(showlist_path, 'w', encoding='utf8') as file:
-            json.dump(showlist, file, sort_keys=True, indent=4)
-    episode_prev = list(episodes.keys())[episode_index - 1]
-    episode = episodes[episode_key]
-    # subprocess.run(["kitty", "icat", episode['small_screenshot']])
+    return episode_key, episodes[episode_key]
+
+def safe(showlist):
+    showlist_path = f'{os.path.dirname(__file__)}/{SHOWLIST_FILE}'
+    with open(showlist_path, 'w', encoding='utf8') as file:
+        json.dump(showlist, file, sort_keys=True, indent=4)
+
+def main():
+    # Arguments
+    show = None
+    args = sys.argv[1:]
+    while len(args) > 0:
+        if args[0] in [ '--show', '-s' ]:
+            show = args[1]
+            args = args[2:]
+            continue
+        print(f'Can not parse {args[0]}')
+        args = args[1:]
+
+    showlist = load()
+
+    show_index, show = select_show(showlist)
+    if show:
+        print(show['name'])
+    else:
+        print('No show selected')
+        safe(showlist)
+        return
+
+    episode_key, episode = select_episode(show)
+    if episode:
+        print(nice_se(episode_key))
+    else:
+        print('No episode selected')
+        safe(showlist)
+        return
+
+    # Saving changes
+    showlist[show_index]['season'] = episode['season']
+    showlist[show_index]['episode'] = episode['episode']
+    safe(showlist)
+
+    # Start torrent/magnet link
     subprocess.call(['xdg-open', episode['magnet_url']])
 
 
